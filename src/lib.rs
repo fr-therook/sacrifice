6e82a1076863c4acaded3003494d19ecb6eb7b8b
bhat <encomblackhat@gmail.com>
2023-04-03 16:23:15 -0700

    rel 0.1.8.
    changelog:
    * expose shakmaty::{File, Rank}

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use shakmaty::{fen::Fen, CastlingMode};
 pub use shakmaty::{Chess, Position};
-pub use shakmaty::{Color, Move, Role, Square, Piece};
+pub use shakmaty::{Color, Move, Role, File, Rank, Square, Piece};
 
 mod game;
 pub use game::Game;
