17d407fad482e1b6dc6ba61065441d063dff504f
bhat <encomblackhat@gmail.com>
2023-04-10 18:24:03 -0700

    rel 0.1.9.
    changelog:
    * expose shakmaty::san.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub use shakmaty::{fen::Fen, CastlingMode};
+pub use shakmaty::{san::San, san::SanPlus};
 pub use shakmaty::{Chess, Position};
 pub use shakmaty::{Color, Move, Role, File, Rank, Square, Piece};
 
